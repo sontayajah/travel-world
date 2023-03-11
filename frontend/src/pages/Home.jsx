0869e0fbc76cbf1f81e38230b89508d4ca94af41
sontayajah <sontaya.jahm@gmail.com>
2023-03-11 14:52:52 +0700

    Travel World / Credit: 'Coding With Muhib' on Youtube

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import '../styles/home.css'
+import { Container, Row, Col } from 'reactstrap'
+import heroImg from '../assets/images/hero-img01.jpg'
+import heroImg02 from '../assets/images/hero-img02.jpg'
+import heroVideo from '../assets/images/hero-video.mp4'
+import worldImg from '../assets/images/world.png'
+import Subtitle from '../shared/Subtitle'
+
+import SearchBar from '../shared/SearchBar'
+import ServiceList from '../services/ServiceList'
+
+const Home = () => {
+  return (
+    <>
+      <section>
+        <Container>
+          <Row>
+            <Col lg='6'>
+              <div className='hero__content'>
+                <div className="hero__subtitle d-flex align-items-center">
+                  <Subtitle subtitle={'Know Before You Go'} />
+                  <img src={worldImg} alt='' />
+                </div>
+                <h1>Traveling opens the door to creating <span className="highlight">memories</span></h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sit enim magnam eum error distinctio perferendis ea aliquam quod sequi cum temporibus, autem hic assumenda eligendi architecto itaque excepturi doloremque!</p>
+              </div>
+            </Col>
+
+            <Col lg='2'>
+              <div className='hero__img-box'>
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+            <Col lg='2'>
+              <div className='hero__img-box mt-4'>
+                <video src={heroVideo} alt="" controls />
+              </div>
+            </Col>
+            <Col lg='2'>
+              <div className='hero__img-box mt-5'>
+                <img src={heroImg02} alt="" />
+              </div>
+            </Col>
+            <SearchBar />
+          </Row>
+        </Container>
+      </section>
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg='3'>
+              <h5 className="services__subtitle">What we serve</h5>
+              <h2 className="services__title">We offer our best services</h2>
+            </Col>
+            <ServiceList />
+          </Row>
+        </Container>
+      </section>
+    </>
+  )
+}
+
+export default Home
